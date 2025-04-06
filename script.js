f6e2cab524e34206e2c9e39411158078f3ce99ea
avilazdudu <avilazdudu@gmail.com>
2025-04-05 21:13:11 -0300

    v.01

---
+++ script.js
@@ -0,0 +1,5 @@
+let OuroBranco = ["Ouro Branco's Easter Egg", 45,90, quantity = 1]
+let SonhoDeValsa  = ["Sonho de Valsa's Easter Egg", 39,99, quantity = 1]
+let FerreroColletion = ["FerreroCollection's Easter Egg", 59,90, quantity = 1]
+let Products = [OuroBranco, SonhoDeValsa, FerreroColletion]
+console.log(Products)
