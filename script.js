6c7afef951ef385084a83c9e30422a110bc079c6
avilazdudu <avilazdudu@gmail.com>
2025-04-06 15:10:52 -0300

    Stylesheet

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let OuroBranco = ["Ouro Branco's Easter Egg", 45,90, quantity = 1]
+let cart = document.getElementById("cart")
+
+let OuroBranco = `<div class="row">
+<img src="./img/OuroBranco.png" alt="Ouro Branco's Easter Egg" class="h-100 w-75 bg-light col-12  product-box-img">
+<span class="h6 col-12 d-flex justify-content-start m-3">Ouro Branco's Easter Egg - 100g</span>
+<span class="col-6 d-flex m-3"><strong>R$ 45,90</strong></span>
+<button class="col-3 ml-4 mr-3 mt-2 mb-3 d-flex justify-content-center  btn btn-success btnCompra">Buy</button>
+</div>`
 let SonhoDeValsa  = ["Sonho de Valsa's Easter Egg", 39,99, quantity = 1]
 let FerreroColletion = ["FerreroCollection's Easter Egg", 59,90, quantity = 1]
 let Products = [OuroBranco, SonhoDeValsa, FerreroColletion]
-console.log(Products)
+
