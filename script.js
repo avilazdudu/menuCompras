c2e1f50f0ad451c5df2accc4a697e888391b66bc
avilazdudu <avilazdudu@gmail.com>
2025-04-07 20:34:36 -0300

    .

--- script.js
+++ script.js
@@ -27,17 +27,23 @@ const valorNovoOuro = () => {
     valorOuro.innerHTML = numeroOuro;
 }
 let numeroOuro = 0;
+let quantidadeOuro = 0;
 
 botaoMaisOuro.addEventListener('click', () => {
     numeroOuro += 1;
     valorNovoOuro();
+    quantidadeOuro += 1;
 });
 
 botaoMenosOuro.addEventListener('click', () => {
     if (numeroOuro > 0) {
         numeroOuro -= 1;
         valorNovoOuro();
+    };
+    if (quantidadeOuro > 0) {
+        quantidadeOuro -= 1;
     }
+    globalThis.quantidadeOuro = quantidadeOuro;
 });
 
 const valorSonho = document.getElementById('valueQuantitySonhoDeValsa');
@@ -49,16 +55,21 @@ const valorNovoSonho = () => {
 }
 
 let numeroSonho = 0;
+let quantidadeSonho = 0;
 
 botaoMaisSonho.addEventListener('click', () => {
     numeroSonho += 1;
     valorNovoSonho();
+    quantidadeSonho += 1;
 });
 
 botaoMenosSonho.addEventListener('click', () => {
     if (numeroSonho > 0) {
         numeroSonho -= 1;
         valorNovoSonho();
+    }
+    if (quantidadeSonho > 0) {
+        quantidadeSonho -= 1;
     }
 });
 
@@ -71,10 +82,12 @@ const valorNovoFerrero = () => {
 }
 
 let numeroFerrero = 0;
+let quantidadeFerrero = 0;
 
 botaoMaisFerrero.addEventListener('click', () => {
     numeroFerrero += 1;
     valorNovoFerrero();
+    quantidadeFerrero += 1;
 });
 
 botaoMenosFerrero.addEventListener('click', () => {
@@ -82,24 +95,18 @@ botaoMenosFerrero.addEventListener('click', () => {
         numeroFerrero -= 1;
         valorNovoFerrero();
     }
-});
-
-//botaoMais.forEach((element) => {
-    //element.addEventListener('click', () => {
-        //numero++;
-        //valor.forEach((el) => {
-      //      el.innerHTML = numero;
-    //    });
-  //  });
-//});  //Error;
-
+    if (quantidadeFerrero > 0) {
+        quantidadeFerrero -= 1;
+    }
+});     
 
 const produtcs = [
     {
         id: 1,
         name: "Ouro Branco's Easter Egg",
         price: 45.90,
-        image: "./img/OuroBranco.png"
+        image: "./img/OuroBranco.png",
+        quantity: quantidadeOuro
 
     },
     {
@@ -115,5 +122,5 @@ const produtcs = [
         image: "./img/FerreroRocher.png"
     }
 ]
-console.log(produtcs[2].price.toFixed(2)) // Test to see if the array is working
+console.log(produtcs[1].quantity) // Test to see if the array is working
 
